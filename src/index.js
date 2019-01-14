f77af73aa485b0b2bef91c4ee6d0709a8f63e206
Nelson Chang <nelsonschang@gmail.com>
2019-01-13 21:48:35 -0600

    rm serviceWorker

--- src/index.js
+++ src/index.js
@@ -6,8 +6,3 @@ import Home from './pages/Home';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
