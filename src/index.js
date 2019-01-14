64c1f0b34de44819d836cc199167b7319309b789
Nelson Chang <nelsonschang@gmail.com>
2019-01-13 22:00:53 -0600

    setup SPA support for gh-pages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import './scss/index.scss';
 import './scss/reset.scss';
 import Home from './pages/Home';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
