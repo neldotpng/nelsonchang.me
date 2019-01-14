c264fc2aa47688de9f9454b9f23dbe28ebab9eb2
Nelson Chang <nelsonschang@gmail.com>
2019-01-13 20:38:01 -0600

    Updates

---
+++ precache-manifest.0752c18cecf68700881c2d8cfc8c381b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "19c17f3fd6abb9b9f11f",
+    "url": "/static/js/main.19c17f3f.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "19c17f3fd6abb9b9f11f",
+    "url": "/static/css/main.bbab9e2c.chunk.css"
+  },
+  {
+    "revision": "ecb9b32eda7255e193d718f0ef249aa2",
+    "url": "/index.html"
+  }
+];
