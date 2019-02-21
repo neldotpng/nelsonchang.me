8d82d16b26a551abba9c88857c79353ed1e79ff7
Nelson Chang <nelsonschang@gmail.com>
2019-02-21 01:59:29 -0600

    animated canvas text move / blur onMouseMove

--- src/components/shared/Canvas.js
+++ src/components/shared/Canvas.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
+import debounce from '../../functions/debounce';
+
 class Canvas extends Component {
   state = {
     width: window.innerWidth,
     height: window.innerHeight,
+    yPos: 0,
   }
 
-  componentDidMount() {
+  drawCanvas = () => {
     this.dpi = window.devicePixelRatio;
 
     this.canvas = document.getElementById('canvas');
@@ -14,15 +17,50 @@ class Canvas extends Component {
     this.canvas.setAttribute('height', this.state.height * this.dpi);
 
     this.ctx = this.canvas.getContext('2d');
-    this.ctx.font = '150px Black Han Sans';
-    this.ctx.fillText('장수영', 10, 150);
+    this.ctx.textAlign = 'center';
+    this.ctx.textBaseline = 'middle';
+    this.ctx.font = `${this.state.height}px Black Han Sans`;
+  }
+
+  drawText = (str) => {
+    const text = str.split('');
+    const mi = (text.length - 1) / 2;
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.filter = `blur(${Math.abs(this.state.yPos / 20)}px)`;
+
+    text.forEach((char, i) => {
+      const mult = i - mi;
+      const xPos = (this.ctx.measureText(char).width * mult) + this.state.width;
+      this.ctx.fillText(char, xPos, this.state.height + (this.state.yPos * mult));
+    });
+  }
+
+  animate = debounce(() => {
+    this.drawText('장수영');
+    requestAnimationFrame(this.animate);
+  }, 1000 / 45)
+
+  onMouseMove = (e) => {
+    const yPos = this.state.height / 2 - e.pageY;
+    console.log(e.pageY);
+    this.setState({ yPos });
+  }
+
+  init = () => {
+    this.drawCanvas();
+    this.animate();
+  }
+
+  componentDidMount() {
+    this.init();
   }
 
   render() {
     return (
       <canvas
         id="canvas"
-        className="shared__canvas"/>
+        className="shared__canvas"
+        onMouseMove={this.onMouseMove} />
     )
   }
 }
