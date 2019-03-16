7ab9d4ab563f2eb8cd47bb7fe59e7aecb1581fd4
Nelson Chang <nelsonschang@gmail.com>
2019-03-16 14:55:44 -0500

    add nav

--- src/components/shared/Nav.js
+++ src/components/shared/Nav.js
@@ -7,6 +7,17 @@ class Nav extends Component {
       <nav className="nav">
         <div className="nav__container">
           <Link to="/" className="nav__home">N.</Link>
+
+          <div className="nav__menu">
+            <div role="button" className="nav__menuButton">
+              <span className="nav__menuLabel">Menu</span>
+              <div className="nav__hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+          </div>
         </div>
       </nav>
     );
