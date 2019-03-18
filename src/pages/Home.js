da63d4cab1bd31b4ff03a1fa3102edebb3b83e2d
Nelson Chang <nelsonschang@gmail.com>
2019-03-17 22:11:44 -0500

    homepage setup wip

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,20 @@ class Home extends Component {
         <header className="home__header">
           <div className="home__headerText">
             <h1 className="home__h1">
-              <span>My name is</span>
-              <span className="home__name">Nelson Chang</span>
+              <span>Hey.</span>
+              <div>I&rsquo;m <span className="home__name">Nelson.</span></div>
             </h1>
             <p className="home__sub">
-              I&rsquo;m a former Pre-Med turned Psych major, and now I work as a front-end developer.
+              I&rsquo;m a Chicago-based (soon-to-be NYC-based)<br/> creative technologist / front-end developer.
             </p>
+          </div>
+          <div className="home__scroll">
+            <div className="home__scrollContainer">
+              <span>Scroll</span>
+              <div className="arrow__down">
+                <div className="arrow__caret" />
+              </div>
+            </div>
           </div>
         </header>
       </div>
