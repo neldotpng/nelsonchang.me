fddf29d2e12938cf839ca376807825e68ddc5cc5
Nelson Chang <nelsonschang@gmail.com>
2019-03-19 00:29:50 -0500

    update nav and home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,21 +6,13 @@ class Home extends Component {
       <div className="home">
         <header className="home__header">
           <div className="home__headerText">
-            <h1 className="home__h1">
-              <span>Hey.</span>
-              <div>I&rsquo;m <span className="home__name">Nelson.</span></div>
+            <h1 className="home__h1 h1">
+              <span>Hi.</span>
+              <div>I’m <span className="home__name">Nelson.</span></div>
             </h1>
             <p className="home__sub">
-              I&rsquo;m a Chicago-based (soon-to-be NYC-based)<br/> creative technologist / front-end developer.
+              I’m a Chicago-based (soon-to-be NYC-based)<br/> creative technologist / front-end developer.
             </p>
-          </div>
-          <div className="home__scroll">
-            <div className="home__scrollContainer">
-              <span>Scroll</span>
-              <div className="arrow__down">
-                <div className="arrow__caret" />
-              </div>
-            </div>
           </div>
         </header>
       </div>
