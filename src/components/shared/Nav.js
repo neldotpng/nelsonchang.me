88d288e7a240c3519640b451f95ac88c39e0f11f
Nelson Chang <nelsonschang@gmail.com>
2019-03-23 20:44:42 -0500

    update nav styling overlay

--- src/components/shared/Nav.js
+++ src/components/shared/Nav.js
@@ -24,7 +24,9 @@ class Nav extends Component {
     return (
       <nav className="nav">
         <div className={containerClasses}>
-          <Link to="/" className="nav__link nav__home">N.</Link>
+          <Link to="/" className="nav__link nav__home">
+            <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><g id="OVERLAP_3_"><path id="XMLID_539_" class="st0" d="M0-.1h100V7H0z"/><path id="XMLID_538_" class="st0" d="M13.3 13.1H100v7.1H13.3z"/><path id="XMLID_537_" class="st0" d="M0 79.4h86.7v7.1H0z"/><path id="XMLID_536_" class="st0" d="M0 92.6h100v7.1H0z"/><path id="XMLID_535_" class="st0" d="M13.3 23.3V13.1h7.2v10.2"/><path id="XMLID_534_" class="st0" d="M7.2 49.9v23.4H0V49.9"/><path id="XMLID_533_" class="st0" d="M0 23.3V-.1h7.2v23.4"/><path id="XMLID_532_" class="st0" d="M20.5 49.9V60h-7.2V49.9"/><path id="XMLID_531_" class="st0" d="M100 76.5v23.4h-7.1V76.5"/><path id="XMLID_530_" class="st0" d="M92.9 49.7V26.4h7.1v23.3"/><path id="XMLID_529_" class="st0" d="M86.8 76.5v10h-7.2v-10"/><path id="XMLID_528_" class="st0" d="M79.6 49.7V39.6h7.2v10.1"/><g id="_x34__3_"><path id="XMLID_19_" class="st0" d="M0 66.1h100v7.1H0z"/></g><g id="_x33__3_"><path id="XMLID_17_" class="st0" d="M13.3 52.9H100V60H13.3z"/></g><g id="_x32__3_"><path id="XMLID_15_" class="st0" d="M0 39.6h86.7v7.1H0z"/></g><g id="_x31__3_"><path id="XMLID_13_" class="st0" d="M0 26.4h100v7.1H0z"/></g></g></svg>
+          </Link>
 
           <div
             role="button"
@@ -39,29 +41,25 @@ class Nav extends Component {
         </div>
 
         <div className={menuClasses}>
-          <svg viewBox="0 0 100 100" width="0" className="nav__menuBackground" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="50" cy="50" r="50"/>
-          </svg>
-
           <div className="nav__menuContainer">
             <ul className="nav__items">
               <li className="nav__item">
-                <Link to="/#" className="nav__menuLink">
+                <Link to="/marquee-sports" className="nav__menuLink">
                   Marquee Sports
                 </Link>
               </li>
               <li className="nav__item">
-                <Link to="/#" className="nav__menuLink">
+                <Link to="/odyssey" className="nav__menuLink">
                   Odyssey
                 </Link>
               </li>
               <li className="nav__item">
-                <Link to="/#" className="nav__menuLink">
+                <Link to="/trunk-club" className="nav__menuLink">
                   Trunk Club
                 </Link>
               </li>
               <li className="nav__item">
-                <Link to="/#" className="nav__menuLink">
+                <Link to="/ar-vr" className="nav__menuLink">
                   AR/VR
                 </Link>
               </li>
